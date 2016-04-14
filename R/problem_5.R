e8dd3836c743d0826a265b9cbd87218cbdecf513
Pablo Areán <pabloarean@gmail.com>
2016-04-14 14:26:06 +0200

    problem 5 in R

---
+++ R/problem_5.R
@@ -0,0 +1,21 @@
+# Problem 5: smallest multiple
+# https://projecteuler.net/problem=5
+
+smallest_multiple <- function(n){
+    result <- 1
+    for(i in 2:n){
+        result <- lcm(result, i)
+    }
+    result
+}
+lcm <- function(x, y){
+    x * y / gcd(x, y)
+}
+gcd <- function(x, y){
+  r <- x%%y
+  return(ifelse(r, gcd(y, x%%y), y))
+}
+
+# Result:
+# > smallest_multiple(20)
+# 232792560
